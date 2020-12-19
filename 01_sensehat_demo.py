0f67df96807427da96c0a804e646058f7483af51
Peng Gu <penggu@gmail.com>
2020-12-19 15:05:56 -0800

    modularize demo 01

--- 01_sensehat_demo.py
+++ 01_sensehat_demo.py
@@ -2,29 +2,38 @@ from sense_hat import SenseHat
 from time import sleep
 from random import randint
 
-
-def show_letter():
-    sense = SenseHat()
+def show_digits(sense):
     for i in range(10):
         c = chr(ord('0') + i)
         r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
         sense.show_letter(c, (r, g, b))
         sleep(1)
+
+def show_lower(sense):
     for i in range(26):
         c = chr(ord('a') + i)
         r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
         sense.show_letter(c, (r, g, b))
         sleep(1)
+
+def show_upper(sense):
     for i in range(26):
         c = chr(ord('A') + i)
         r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
         sense.show_letter(c, (r, g, b))
         sleep(1)
+
+def show_letters():
+    sense = SenseHat()
+    sense.clear()
+    show_digits(sense)
+    show_lower(sense)
+    show_upper(sense)
     sense.clear()
 
 def main():
     for i in range(1):
-        show_letter()
+        show_letters()
 
 if __name__ == "__main__":
     main()
